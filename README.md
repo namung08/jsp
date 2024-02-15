9226ffc23fe412538ead1add536fdef6a9179b38
namung08 <namung08@gmail.com>
2024-02-15 14:18:27 +0900

    edit readme and add myservlet2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # jsp
 
+// web.xml에 복잡한 고드륵 안 적기 위한 서블랫
+@WebServlet("/cnt")
+
+// 한글을 출력하기 위한 코드
+    arg1.setCharacterEncoding("UTF-8");
+    arg1.setContentType("text/html; charset=utf-8");
+
+// out 생성
+PrintWriter out = arg1.getWriter();
--- workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet2.java
+++ workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet2.java
@@ -8,20 +8,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet("/hello")
-public class MyServlet extends HttpServlet{
+@WebServlet("/cnt")
+public class MyServlet2 extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// 한글을 출력하기 위한 코드
 		arg1.setCharacterEncoding("UTF-8");
-		arg1.setContentType("text/html; charset=utf-8");
-		//
-		PrintWriter out = arg1.getWriter();
-		for(int i=0;i<100;i++) {
-			out.println((i+1)+": 안녕 servlet</br>");
-		}
-		out.println("Hello Servlet");
+	    arg1.setContentType("text/html; charset=utf-8");
+		// TODO Auto-generated method stub
+	    PrintWriter out = arg1.getWriter();
+	    out.print("안녕 servlet");
 		
 	}
 }
