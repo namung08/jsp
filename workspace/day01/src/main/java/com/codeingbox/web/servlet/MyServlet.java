ab157bd9fe04b35208b56ff7ca3b0396f891e4b3
namung08 <namung08@gmail.com>
2024-02-15 14:04:41 +0900

    for servlet

--- workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet.java
+++ workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet.java
@@ -4,16 +4,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+@WebServlet("/hello")
 public class MyServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = arg1.getWriter();
+		for(int i=0;i<100;i++) {
+			out.println((i+1)+": hello servlet</br>");
+		}
 		out.println("Hello Servlet");
-		super.service(arg0, arg1);
+		
 	}
 }
