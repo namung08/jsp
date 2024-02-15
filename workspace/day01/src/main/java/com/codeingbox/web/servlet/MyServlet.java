82884d51ff20c1ed9422cfe86b637ea9e6a5584b
namung08 <namung08@gmail.com>
2024-02-15 12:28:56 +0900

    setting servlet

--- workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet.java
+++ workspace/day01/src/main/java/com/codeingbox/web/servlet/MyServlet.java
@@ -1,7 +1,19 @@
 package com.codeingbox.web.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet{
-
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = arg1.getWriter();
+		out.println("Hello Servlet");
+		super.service(arg0, arg1);
+	}
 }
