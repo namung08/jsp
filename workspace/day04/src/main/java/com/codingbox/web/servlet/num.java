f1d33cc1acc26028342a7e205c148d86875d05ae
namung08 <namung08@gmail.com>
2024-02-19 15:15:01 +0900

    edit doPost

--- workspace/day04/src/main/java/com/codingbox/web/servlet/num.java
+++ workspace/day04/src/main/java/com/codingbox/web/servlet/num.java
@@ -21,6 +21,10 @@ public class num extends HttpServlet{
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
 		
+		PrintWriter out = resp.getWriter();
+		out.println(num1+num2);
 	}
 }
