4b56fa90959488d5639069e772b2ecc079c12f09
namung08 <namung08@gmail.com>
2024-02-19 15:07:38 +0900

    create nnumber.jsp num.java

---
+++ workspace/day04/src/main/java/com/codingbox/web/servlet/num.java
@@ -0,0 +1,14 @@
+package com.codingbox.web.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/num")
+public class num extends HttpServlet{
+	
+}
--- workspace/day04/src/main/webapp/number.jsp
+++ workspace/day04/src/main/webapp/number.jsp
@@ -7,6 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<form action="num" method="get">
+		첫번쨰 정수<input type="text" name="num1"><br>
+		두번쨰 정수<input type="text" name="num1"><br>
+		<input type="submit">
+	</form>
 </body>
 </html>
