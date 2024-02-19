0d4ff6ec1e16c41cb692d8dbae78e8ed8e44764a
namung08 <namung08@gmail.com>
2024-02-19 15:23:50 +0900

    i dont know

--- workspace/day04/src/main/java/com/codingbox/web/servlet/num.java
+++ workspace/day04/src/main/java/com/codingbox/web/servlet/num.java
@@ -13,15 +13,18 @@ import jakarta.servlet.http.HttpServletResponse;
 public class num extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int num1 = Integer.parseInt(req.getParameter("num1"));
-		int num2 = Integer.parseInt(req.getParameter("num2"));
-		
-		PrintWriter out = resp.getWriter();
-		out.println(num1+num2);
+		doProcess(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Post 요청 도착");
-		doGet(req, resp);
+		
+		doProcess(req, resp);
+	}
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		
+		PrintWriter out = response.getWriter();
+		out.println(num1+num2);
 	}
 }
