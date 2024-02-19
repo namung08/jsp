9572fc8b2fde9d6c29488e4c75aa9ad9038cda5e
namung08 <namung08@gmail.com>
2024-02-19 14:46:47 +0900

    super arg0,1

--- workspace/day04/src/main/java/com/codingbox/web/servlet/Calculator.java
+++ workspace/day04/src/main/java/com/codingbox/web/servlet/Calculator.java
@@ -19,6 +19,8 @@ public class Calculator extends HttpServlet{
 		} else if(arg0.getMethod().equals("POST")) {
 			System.out.println("POST 요청입니다.");
 		}
+		// 부모의 service를 호출해 주면 doPost, doGet 메서드를 반드시 재 구현해 주어야 한다.
+		super.service(arg0, arg1);
 	}
 
 }
