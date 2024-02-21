719529f39d379785774fe2170f403faf7c8cef67
namung08 <namung08@gmail.com>
2024-02-21 12:23:26 +0900

    setting servlet

--- workspace/day06/src/main/java/com/codingbox/web/servlet/JoinServlet.java
+++ workspace/day06/src/main/java/com/codingbox/web/servlet/JoinServlet.java
@@ -1,5 +1,18 @@
 package com.codingbox.web.servlet;
 
-public class JoinServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/join")
+public class JoinServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 }
