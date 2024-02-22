e4d2a6b2b3a95c48b2ef6afbf3d1c0a7cae23c74
namung08 <namung08@gmail.com>
2024-02-22 11:36:50 +0900

    ?

--- workspace/day07/src/main/java/com/codingbox/web/dao/MemberDAO.java
+++ workspace/day07/src/main/java/com/codingbox/web/dao/MemberDAO.java
@@ -5,38 +5,62 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.jar.Attributes.Name;
 
 import com.codingbox.web.dbconnection.DBConnection;
+import com.codingbox.web.dto.MemberDTO;
 
 public class MemberDAO {
-	Connection conn;
-	PreparedStatement pstm;
-	ResultSet rs;
-	
-	// age찾기
-	public int getAge(String name) {
-		int age = 0;
-		//db처리
-		try {
-			conn = DBConnection.getConnection();
-			String sql = "SELECT * FROM MEMBER WHERE name=?";
-			pstm = conn.prepareStatement(sql);
-			pstm.setString(1, name);
-			rs = pstm.executeQuery();
-			
-			if(rs.next()) {
-				age = rs.getInt("age");
-			}
-		}catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("SQL 예외 발생");
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("예외 발생");
-		}
-		return age;
-	}
-	
-	// 전체 검색
-	public ArrayList<MemberDTO>
+   Connection conn;
+   PreparedStatement pstm;
+   ResultSet rs;
+   
+   // age찾기
+   public int getAge(String name) {
+      int age = 0;
+      //db처리
+      try {
+         conn = DBConnection.getConnection();
+         String sql = "SELECT * FROM MEMBER WHERE name=?";
+         pstm = conn.prepareStatement(sql);
+         pstm.setString(1, name);
+         rs = pstm.executeQuery();
+         
+         if(rs.next()) {
+            age = rs.getInt("age");
+         }
+      }catch (SQLException e) {
+         e.printStackTrace();
+         System.out.println("SQL 예외 발생");
+      }catch (Exception e) {
+         e.printStackTrace();
+         System.out.println("예외 발생");
+      }
+      return age;
+   }
+
+   // 전체 검색
+   public ArrayList<MemberDTO> selectAll(){
+      String sql = "SELECT * FROM MEMBER";
+      ArrayList<MemberDTO> result = new ArrayList<MemberDTO>();
+      try {
+         conn = DBConnection.getConnection();
+         pstm = conn.prepareStatement(sql);
+         rs = pstm.executeQuery();
+         while(rs.next()) {
+        	 
+         }
+      }catch (SQLException e) {
+         e.printStackTrace();
+         System.out.println("SQL 예외 발생");
+      }catch (Exception e) {
+         e.printStackTrace();
+         System.out.println("예외 발생");
+      }
+      
+      return null;
+   }
+
+
+
 }
