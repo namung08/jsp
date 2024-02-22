dc7e8de38515fcaf7e4aa94f827d077373c84cb5
namung08 <namung08@gmail.com>
2024-02-22 10:12:45 +0900

    connection

--- workspace/day07/src/main/java/com/codingbox/web/dbconnection/DBConnection.java
+++ workspace/day07/src/main/java/com/codingbox/web/dbconnection/DBConnection.java
@@ -5,15 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	public static Connection getConnection() throws SQLException {
+	public static Connection getConnection() {
+
 		Connection conn = null; // Connection 객체를 초기화
-		// 데이터베이스 연결을 위한 드라이버, URL, 사용자 이름, 암호 설정
-		String driver = "oracle.jdbc.driver.OracleDriver"; // Oracle JDBC 드라이버 클래스
-		String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스 연결 URL
-		String user = "jsp"; // 데이터베이스 사용자 이름
-		String pass = "jsp"; // 데이터베이스 암호
-		conn = DriverManager.getConnection(url, user, pass);
-		
+		try {
+			// 데이터베이스 연결을 위한 드라이버, URL, 사용자 이름, 암호 설정
+			String driver = "oracle.jdbc.driver.OracleDriver"; // Oracle JDBC 드라이버 클래스
+			String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스 연결 URL
+			String user = "jsp"; // 데이터베이스 사용자 이름
+			String pass = "jsp"; // 데이터베이스 암호
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("DB 접속 오류");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("알 수 없는 오류");
+		}
+
 		return conn;
 	}
 }
--- workspace/day07/src/main/webapp/age_db.jsp
+++ workspace/day07/src/main/webapp/age_db.jsp
@@ -14,14 +14,6 @@
 	<%
 	String name = request.getParameter("name");
 	
-	Connection conn = null; // Connection 객체를 초기화
-	// 데이터베이스 연결을 위한 드라이버, URL, 사용자 이름, 암호 설정
-	String driver = "oracle.jdbc.driver.OracleDriver"; // Oracle JDBC 드라이버 클래스
-	String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스 연결 URL
-	String user = "jsp"; // 데이터베이스 사용자 이름
-	String pass = "jsp"; // 데이터베이스 암호
-	String sql = null;	
-	conn = DriverManager.getConnection(url, user, pass);
 	%>
 </body>
 </html>
